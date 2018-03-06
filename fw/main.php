b7491eec36d6b8fa6e943d9c85dc6f73d20b5757
morswin22 <xmorswinx@gmail.com>
2018-03-06 22:38:56 +0100

    move readme

--- fw/main.php
+++ fw/main.php
@@ -1,30 +1,5 @@
 <?php
 
-// Framework readme
-//
-// Initialize framework in file with: 
-//     include('path_to_common/main.php'); 
-//     $fw->set('pagename');
-//
-// Output default head elements: 
-//     $fw->commonMeta()
-//     $fw->title()
-//     $fw->commonLink()
-//     $fw->commonScript()
-//
-// Output specific head elements for a pagename:
-//     $fw->fullMeta()
-//     $fw->title()
-//     $fw->fullLink()
-//     $fw->fullScript()
-//
-// Output html code:
-//     $fw->header()
-//     $fw->navbar()
-//     $fw->footer()
-//
-// Made With ♥ By Patryk Janiak (https://morswin22.github.io/)
-
 include_once('fw.php');
 
 $fw = new Framework();
