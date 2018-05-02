80eba8d2354518b452b3486ee277d6c4cf107482
morswin22 <xmorswinx@gmail.com>
2018-05-02 17:32:19 +0200

    Finishing index.php

--- fw/main.php
+++ fw/main.php
@@ -4,6 +4,7 @@ include_once('fw.php');
 
 $fw = new Framework('http://localhost/ikc_30-04-2018');
 
+$fw->add_component('3rd-party', '{{domain_url}}/fw/components/3rd-party.html');
 $fw->add_component('navbar', '{{domain_url}}/fw/components/navbar.html');
 
 ?>
--- index.php
+++ index.php
@@ -5,14 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
     <title>INTRO-KSERO-CENTRUM</title>
+    <?=$fw->component('3rd-party')?>
     <link href="css/main.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
 </head>
 <body>
     
+    <?=$fw->component('navbar')?>
+
     <div class="container">
 
-        <?=$fw->component('navbar')?>
+        <h1 id="brand">INTRO-KSERO-CENTRUM</h1>
+
+        <a href="services.php" class="button" id="main">
+            <i class="material-icons">&#xE85D;</i><span>USŁUGI</span>
+        </a>
+        <a href="location.php" class="button">
+            <i class="material-icons">&#xE0C8;</i><span>LOKALIZACJA</span>
+        </a>
+        <a href="contact.php" class="button">
+            <i class="material-icons">&#xE158;</i><span>KONTAKT</span>
+        </a>
 
     </div>
 
